5e68df9abe2f7e523ddf618ac3f438853e15cda6
SomashekarMH <somu.goldeneye@gmail.com>
2026-01-22 09:01:10 +0530

    added line

--- somu.java
+++ somu.java
@@ -1,5 +1,6 @@
 public class somu {
     public static void main(String[] args) {
         System.out.println("Welcome somu to java");
+        System.out.println("Welcome somu to World of Java");
     }
 }
